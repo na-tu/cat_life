ba915518bd7adfe99ee92831817f892ff40d3379
na-tu <161991245+na-tu@users.noreply.github.com>
2024-08-19 14:21:30 +0000

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'homes/top'
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
