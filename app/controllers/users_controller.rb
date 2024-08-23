4aff8fc6d477154bffcc4e9a582fdc89df793c4d
na-tu <161991245+na-tu@users.noreply.github.com>
2024-08-23 14:00:20 +0000

    7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,43 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
+    @user = User.find(params[:id])
+    @post = Post.new
+    @post.user_id = current_user.id
+    @posts = @user.posts
+  end
+
+  def index
+    @user = current_user
+    @users = User.all
+    @post = Post.new
   end
 
   def edit
+    user = User.find(params[:id])#editの中だけで確認するために＠はいれていない。
+    if current_user== user
+    @user = User.find(params[:id])
+    else
+    redirect_to user_path(current_user)
+    end
   end
+  
+  def update
+    @user = User.find(params[:id])#ユーザーの取得
+   if @user.update(user_params)#ユーザーのアップデート
+   flash[:notice] = "You have updated user successfully."
+  redirect_to user_path(@user.id)#ユーザーの詳細ページへのパス
+   else
+  render :edit
+   end
+  end
+
+private
+
+ def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+ end
+
+ def is_matching_login_user
+ end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/home/about',to:'homes#about', as:'about'
   root to: 'homes#top'
   
-  resources :users, only: [:show,:edit,:update]
+  resources :users, only: [:show,:edit,:update,:index]
   resources :posts, only: [:new,:create,:index,:show,:edit,:update,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
